862529063eade8c4203b0577bb538a383d3263c8
Schwannden Kuo <schwannden@mobagel.com>
2022-09-23 15:31:35 +0800

    adding a database and simple backend app

--- lesson1/backend/app.py
+++ lesson1/backend/app.py
@@ -1,11 +1,30 @@
 from typing import Optional
 
+from beanie import init_beanie, Document
+from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import FastAPI
 
-app = FastAPI()
+
+class Greeting(Document):
+    message: str
 
 
-@app.get("/")
-async def root(name: str = "world"):
-    return {"message": f"Hello {name}!"}
+async def init():
+    client = AsyncIOMotorClient("mongodb://admin-user:admin-password@192.168.6.77:27017")
+    await init_beanie(database=client.backend, document_models=[Greeting])
+
+
+app = FastAPI(on_startup=[init])
+
+
+@app.post("/")
+async def create(name: str = "world"):
+    message = Greeting(message=f"Hello {name}!")
+    await message.save()
+    return message
+
+@app.get("/all")
+async def get_all():
+    greetings = await Greeting.find().to_list()
+    return greetings
 
